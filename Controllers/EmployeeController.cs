a793560b729ff893ec9e6cb5429bd35c0b85e76a
Snehal Ingale <snehalb5191@gmail.com>
2026-03-23 16:23:33 +0530

    CRUD operations

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -10,8 +10,8 @@ namespace WebAPIForEmployee.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
-       private readonly MyDbContext _dbContext;
-       
+        private readonly MyDbContext _dbContext;
+
 
         public EmployeeController(MyDbContext context)
         {
@@ -21,7 +21,7 @@ namespace WebAPIForEmployee.Controllers
         [Route("GetEmployee")]
         public IActionResult GetAllEmployee()
         {
-           var employees = _dbContext.Employees.ToList();
+            var employees = _dbContext.Employees.ToList();
             return Ok(employees);
         }
 
@@ -29,26 +29,95 @@ namespace WebAPIForEmployee.Controllers
         [Route("GetEmployeeById/{id}")]
         public IActionResult GetEmployeeById(int id)
         {
-            var emp=_dbContext.Find<Employee>(id);
-            if(emp==null)
+            var emp = _dbContext.Find<Employee>(id);
+            if (emp == null)
             {
                 return NotFound();
             }
             return Ok(emp);
         }
 
+
+        //Add Employee details in DB
         [HttpPost]
         [Route("AddEmployee")]
         public IActionResult AddEmployee(Employee emp)
         {
-            
-            if(!ModelState.IsValid)
+
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             _dbContext.Set<Employee>().Add(emp);
             _dbContext.SaveChanges();
             return Ok(emp);
+        }
+
+        //Update Employee details in DB
+        [HttpPut]
+        [Route("UpdateEmployee/{id}")]
+        public IActionResult UpdateEmployee(int id, Employee emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var empl = _dbContext.Employees.FirstOrDefault(e => e.Id == id);
+            if (empl != null)
+            {
+                empl.EmpName = emp.EmpName;
+                empl.Department = emp.Department;
+                empl.Salary = emp.Salary;
+                _dbContext.SaveChanges();
+                return Ok("Employee updated Successfully");
+            }
+            else
+            {
+                return NotFound("Employee Not Found");
+            }
+        }
+        //Delete Employee details in DB
+        [HttpDelete]
+        [Route("DeleteEmployee/{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var emp = _dbContext.Employees.FirstOrDefault(e => e.Id == id);
+            if (emp != null)
+            {
+                _dbContext.Employees.Remove(emp);
+                _dbContext.SaveChanges();
+                return Ok("Employee Deleted Successfully");
+            }
+            else
+            {
+                return NotFound("Employee Not Found");
+            }
+        }
+        //Update only salary of Employee  in DB
+        [HttpPatch]
+        [Route("UpdateEmployeeFields/{id}")]
+        public IActionResult UpdateEmployeeFields(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var emp = _dbContext.Employees.FirstOrDefault(e => e.Id == id);
+            if (emp != null)
+            {
+                if (employee.Salary != 0)
+                {
+                    emp.Salary = employee.Salary;
+                }
+                emp.CreatedDate = DateTime.Now;
+                _dbContext.SaveChanges();
+
+                return Ok("Employee Salary updated successfully");
+            }
+            else
+            {
+                return NotFound("Employee Not Found");
+            }
         }
     }
 
